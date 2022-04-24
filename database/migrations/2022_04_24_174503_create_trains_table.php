e8fcf506e9cc378d144413d07a135f8a42550f54
Michela-DC <micheladelconte@outlook.com>
2022-04-24 21:57:02 +0200

    Added migration and created trains table

---
+++ database/migrations/2022_04_24_174503_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 40);
+            $table->string('stazione_di_partenza', 30);
+            $table->string('stazione_di_arrivo', 30);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno',10);
+            $table->tinyInteger('numero_carrozze')->unsigned;;
+            $table->float('prezzo_biglietto', 4,2);
+            $table->string('in_orario')->default(1);
+            $table->string('cancellato')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
